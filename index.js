e756567d19894db3b4b97e2979ac8160ce264519
Matias Singers <mail@matiassingers.com>
2014-04-25 20:09:45 +0800

    Create initial API + Jade view for testing

---
+++ index.js
@@ -0,0 +1,38 @@
+var express = require('express');
+var Country = require('./model/country.js');
+
+var app = express();
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jade');
+
+app.get('/api/', function(req, res){
+  res.set({'content-type': 'application/json; charset=utf-8'});
+  getItems(function(items){
+    res.send(items);
+  });
+});
+
+app.get('/', function(req, res){
+  getItems(function(items){
+    res.render('index',
+      {
+        data : items
+      }
+    )
+  });
+});
+
+function getItems(callback){
+  Country
+    .find({})
+    .sort('convertedPrice')
+    .exec(function(err, items){
+      if(err)
+        throw err;
+
+      callback(items);
+    });
+}
+
+app.listen(3000);
+console.log('Listening on port 3000...');
