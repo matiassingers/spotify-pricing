3c0de41b475ebc51005560e3702d4de5c44fd544
Matias Singers <mail@matiassingers.com>
2014-05-02 17:42:08 +0800

    Add String.prototype.format to app.js

--- js/app.js
+++ js/app.js
@@ -1,3 +1,12 @@
+String.prototype.format = function () {
+  var formatted = this;
+  for (var i = 0; i < arguments.length; i++) {
+    formatted = formatted.replace(
+      RegExp('\\{' + i + '\\}', 'g'), arguments[i]);
+  }
+  return formatted;
+};
+
 var countries;
 var base;
 
